8c9a984091aab88a182fe317da04d766c29259c8
Eric Jung <118792303+e11b@users.noreply.github.com>
2022-12-21 14:51:38 -0600

    Section 6 assignments and challenges

--- Section6_ControlFlow/src/L65_whileLoopChallenge.java
+++ Section6_ControlFlow/src/L65_whileLoopChallenge.java
@@ -2,12 +2,28 @@ public class L65_whileLoopChallenge {
 
     public static void main(String[] args) {
 
-        int number = 0;
-        while ((number >=5) && (number <= 20)) {
-            if (isEvenNUmber(number)) {
-                System.out.println(number);
+        int number = 4;
+        int endNumber = 20;
+        int count = 0;
+        int countOdd = 0;
+
+        while (number <=20) {
+            number++;
+            if (!isEvenNUmber(number)) {
+                countOdd++;
+                continue;
             }
+            System.out.println(number);
+            count++;
+
+            if (count >= 5) {
+                break;
+            }
+
         }
+
+        System.out.println("Found " + count + " even numbers");
+        System.out.println("Found " + countOdd + " odd numbers");
 
     }
 
