dac84f7e6f87374be201f7d313b42700d5ea34ff
Eric Jung <118792303+e11b@users.noreply.github.com>
2022-12-21 17:14:40 -0600

    L77 and L77

---
+++ Section7_OOP/src/Car.java
@@ -0,0 +1,65 @@
+public class Car {
+
+    private String make = "Porsche";
+    private String model = "718 GTS 4.0";
+    private String color = "Chalk";
+    private int doors = 2;
+    private boolean convertible = false;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void setMake(String make) {
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "porsche", "bmw", "mercedes" -> this.make = make;
+            default -> {
+                this.make = "Unsupported ";
+            }
+        }
+    }
+    public void describeCar() {
+
+    System.out.println(doors + "-Door " +
+            color + " " +
+            make + " " +
+            model + " " +
+            (convertible ? "Convertible" : "" ));
+    }
+
+}
+
--- Section7_OOP/src/Main.java
+++ Section7_OOP/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car = new Car(); //Variable type = car; called "car" = assign a new instance of the Car class
+        car.setMake("Audi");
+        car.setModel("GTS 4.0");
+        car.setDoors(2);
+        car.setConvertible(false);
+        car.setColor("Chalk");
+
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(true);
+        targa.setColor("red");
+        targa.describeCar();
+
     }
 }
