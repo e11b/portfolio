29dfa327ea51d2488823798bc90702228ff30809
Eric Jung <118792303+e11b@users.noreply.github.com>
2022-12-29 12:40:20 -0600

    L86 - POJO,
    L87 - Added implicit POJO tupe and the Record

--- Section7_OOP/src/L86_POJO/Main.java
+++ Section7_OOP/src/L86_POJO/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch(i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -18,6 +18,25 @@ public class Main {
                     "Java Masterclass");
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S923006", "Ann",
+                "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923007", "Bill",
+                "05/11/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        recordStudent.setClassList(recordStudent() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " +
+                pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " +
+                recordStudent.classList());
+
+
+
 
     }
 }
