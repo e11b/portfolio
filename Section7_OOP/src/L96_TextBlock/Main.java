c7970569235205c4ed2f4019507f946f3534dc77
Eric Jung <118792303+e11b@users.noreply.github.com>
2023-01-07 11:29:13 -0600

    L95 string formatting 1

---
+++ Section7_OOP/src/L96_TextBlock/Main.java
@@ -0,0 +1,42 @@
+package L96_TextBlock;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String bulletIt = "Print a Bulleted List: \n" +
+                "\t\u2022 First Point\n" +
+                "\t\t\u2022 sub point";
+
+        System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    \u2022 First Point
+                        \u2022 Sub Point """;
+
+        System.out.println(textBlock);
+
+        int age = 35;
+        System.out.printf("Your age is %d%n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d%n", age, yearOfBirth);
+
+        System.out.printf("Your age is %.2f%n", (float)age);
+
+        for(int i = 1; i <=100_000; i *= 10){
+            System.out.printf("Printing %d %n", i);
+
+        }
+
+//        String formatttedString = String.format("Your age is %d", age);
+//        System.out.println(formatttedString);
+
+        String formatttedString = "Your age is %d".formatted(age);
+        System.out.println(formatttedString);
+
+
+    }
+
+}
