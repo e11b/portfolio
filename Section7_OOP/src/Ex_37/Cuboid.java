33ef66fff672261f38fddf48b9d573e792e17d4e
Eric Jung <118792303+e11b@users.noreply.github.com>
2023-01-08 00:50:25 -0600

    Ex 37 Volume

---
+++ Section7_OOP/src/Ex_37/Cuboid.java
@@ -0,0 +1,22 @@
+package Ex_37;
+
+public class Cuboid extends Rectangle{
+
+    public double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            this.height = 0;
+        } else this.height = height;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getVolume(){
+        return height*getArea();
+    }
+
+}
---
+++ Section7_OOP/src/Ex_37/Rectangle.java
@@ -0,0 +1,29 @@
+package Ex_37;
+
+public class Rectangle {
+
+    public double width;
+    public double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0) {
+            this.width = 0;
+        } else this.width = width;
+
+        if (length < 0 ) {
+            this.length = 0;
+        } else this.length = length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public double getArea(){
+        return width*length;
+    }
+}
